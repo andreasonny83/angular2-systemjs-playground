c1ac5644e7fd6c3c9d0c0c2c2ad69c41207ddb0c
andreasonny83 <andreasonny83@gmail.com>
2017-02-27 20:33:37 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Angular2 SystemJS Playground
+
+> A simple Angular2 boilerplate with SystemJS
+
+Install all the Node dependencies with:
+
+```bash
+$ npm Install
+
+$ yarn install # for Yarn support
+```
+
+The run the project in your browser with:
+
+```bash
+$ npm start
+```
+
+That's it!
---
+++ src/app.ts
@@ -0,0 +1,31 @@
+// Our root app component
+
+import { Component, NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <div>
+      <h2>Hello {{name}}</h2>
+    </div>
+  `,
+})
+export class App {
+  name: string;
+
+  constructor() {
+    this.name = "Angular2";
+  }
+}
+
+@NgModule({
+  imports: [
+    BrowserModule,
+  ],
+  declarations: [
+    App,
+  ],
+  bootstrap: [ App ]
+})
+export class AppModule {}
